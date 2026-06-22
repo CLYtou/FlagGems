57f10db916b9b5bd8fbb12f725e3d7af87954eab
huangyiqun <huangyiqun@mail.bnu.edu.cn>
2026-06-22 07:02:04 +0000

    Restore llama benchmark shapes

--- benchmark/attri_util.py
+++ benchmark/attri_util.py
@@ -29,6 +29,28 @@ DEFAULT_SHAPES = [
 ]
 
 
+# This function is adapted from: https://github.com/pytorch-labs/tritonbench/blob/main/tritonbench/utils/triton_op.py
+def llama_shapes():
+    # batch sizes * seq lengths
+    BS = [2**i for i in range(0, 17)]
+    # attn: wqkv, wo; ffn: w13, w2
+    KN = [
+        (4096, 12288),
+        (4096, 4096),
+        (4096, 22016),
+        (11008, 4096),
+        (8192, 1280),
+        (1024, 8192),
+        (8192, 7168),
+        (3584, 8192),
+        (16384, 2304),
+        (2048, 16384),
+        (16384, 13312),
+        (6656, 16384),
+    ]
+    return [(bs, n, k, None) for bs, (k, n) in itertools.product(BS, KN)]
+
+
 def model_shapes():
     # batch sizes * seq lengths
     BS = [1, 2, 3, 4, 8, 98, 256, 8192]
